3d3f6130ba08beee25ba7aca335aaf18fd723dd0
Susan Fung <fung_susan@hotmail.com>
2026-08-24 17:15:42 -0600

    . e Connected jooq to supabase

--- src/main/java/com/example/application/Application.java
+++ src/main/java/com/example/application/Application.java
@@ -1,13 +1,20 @@
 package com.example.application;
 
+import org.jooq.SQLDialect;
+
 import com.example.application.security.SecurityFilter;
 import com.vaadin.flow.component.dependency.NpmPackage;
 import com.vaadin.flow.component.page.AppShellConfigurator;
 import com.vaadin.flow.server.VaadinServiceInitListener;
 import com.vaadin.flow.theme.Theme;
+import org.jooq.impl.DSL;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.jooq.DSLContext;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
 
 /**
  * The entry point of the Spring Boot application.
@@ -28,5 +35,16 @@ public class Application implements AppShellConfigurator {
     @Bean
     public VaadinServiceInitListener securityFilter() {
         return new SecurityFilter();
+    }
+
+    @Bean
+    public DSLContext dslContext() throws Exception {
+        String url = System.getenv("SUPABASE_JDBC_URL");
+        String user = System.getenv("SUPABASE_DB_USER");
+        String password = System.getenv("SUPABASE_DB_PASSWORD");
+
+        Connection conn = DriverManager.getConnection(url, user, password);
+
+        return DSL.using(conn, SQLDialect.POSTGRES);
     }
 }
